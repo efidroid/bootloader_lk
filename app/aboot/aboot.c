c18f6c63f94704fca0f06fce297965a9593ec856
Sridhar Parasuram <sridhar@codeaurora.org>
2015-07-10 13:33:22 -0700

    app: aboot: Call delete keys when wiping userdata
    Call delete keys api when wiping userdata.
    
    Change-Id: I81fe028a8e5e9618ef561279e6a14cac9f471a7e

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -83,6 +83,7 @@
 #include "board.h"
 #include "scm.h"
 #include "mdtp.h"
+#include "secapp_loader.h"
 
 extern  bool target_use_signed_kernel(void);
 extern void platform_uninit(void);
@@ -2184,6 +2185,10 @@ void cmd_erase_mmc(const char *arg, void *data, unsigned sz)
 			return;
 		}
 	}
+#if VERIFIED_BOOT
+	if(send_delete_keys_to_tz())
+		ASSERT(0);
+#endif
 	fastboot_okay("");
 }
 
