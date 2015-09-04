8fb4f54eb38dedea6be9cbf11f393edbcab3555a
M1cha <sigmaepsilon92@gmail.com>
2015-09-04 13:52:02 +0200

    [EFIDROID] dev: keys: add key queue

--- include/dev/keys.h
+++ include/dev/keys.h
@@ -33,6 +33,9 @@
 #define __DEV_KEYS_H
 
 #include <sys/types.h>
+#include <list.h>
+#include <platform.h>
+#include <assert.h>
 
 /* these are just the ascii values for the chars */
 #define KEY_0		0x30
@@ -84,8 +87,144 @@
 
 #define MAX_KEYS	0x1ff
 
+typedef struct key_event_source key_event_source_t;
+typedef int (*key_event_poll_t)(key_event_source_t* source);
+
+typedef enum {
+    KEYSTATE_RELEASED,
+    KEYSTATE_PRESSED,
+    KEYSTATE_LONGPRESS_WAIT,
+    KEYSTATE_LONGPRESS_RELEASE,
+} keystate_t;
+
+typedef struct {
+	time_t time;
+	bool repeat;
+	bool longpress;
+	keystate_t state;
+
+	bool enable_longpress;
+} keymap_t;
+
+struct key_event_source {
+	struct list_node node;
+	void* pdata;
+
+	time_t last;
+	time_t delta;
+	key_event_poll_t poll;
+	keymap_t keymap[MAX_KEYS];
+};
+
 void keys_init(void);
-void keys_post_event(uint16_t code, int16_t value);
+int keys_poll(void);
+void keys_add_source(key_event_source_t* source);
+int keys_post_event(uint16_t code, int16_t value);
 int keys_get_state(uint16_t code);
+void keys_clear_all(void);
+int keys_get_next(uint16_t* code, uint16_t* value);
+int keys_has_next(void);
+
+static inline int keys_set_report_key(key_event_source_t* source, uint16_t code, uint16_t* pvalue) {
+	int rc = 0;
+	uint16_t value = *pvalue;
+
+	// update key time
+	source->keymap[code].time+=source->delta;
+
+	switch(source->keymap[code].state) {
+		case KEYSTATE_RELEASED:
+			if(value) {
+				// change to pressed
+				if(source->keymap[code].enable_longpress) {
+					source->keymap[code].time=0;
+					source->keymap[code].state = KEYSTATE_LONGPRESS_WAIT;
+				}
+				else {
+					*pvalue = 1;
+					rc = 1;
+					source->keymap[code].time=0;
+					source->keymap[code].state = KEYSTATE_PRESSED;
+				}
+			}
+			break;
+
+		case KEYSTATE_PRESSED:
+			if(!value && source->keymap[code].time>50) {
+				// change to released
+				source->keymap[code].time=0;
+				source->keymap[code].repeat=false;
+				source->keymap[code].longpress = false;
+				source->keymap[code].state = KEYSTATE_RELEASED;
+				*pvalue = 0;
+				rc = 1;
+			}
+
+			// key repeat
+			else if((source->keymap[code].repeat && source->keymap[code].time>=200) || source->keymap[code].time>=500) {
+				*pvalue = 1;
+				rc = 1;
+				source->keymap[code].time=0;
+				source->keymap[code].repeat=true;
+			}
+			break;
+
+		case KEYSTATE_LONGPRESS_WAIT:
+			if(value) {
+				if(!source->keymap[code].longpress && source->keymap[code].time>=500) {
+					if(keys_get_state(KEY_VOLUMEDOWN)) {
+						// report 's'
+						keys_post_event(0x73, 1);
+						keys_post_event(0x73, 0);
+					}
+					else if(keys_get_state(KEY_VOLUMEUP)) {
+						// report 'e'
+						keys_post_event(0x65, 1);
+						keys_post_event(0x65, 0);
+					}
+					else {
+						// report spacebar
+						keys_post_event(32, 1);
+						keys_post_event(32, 0);
+					}
+
+					source->keymap[code].longpress=true;
+				}
+			}
+
+			else {
+				if(!source->keymap[code].longpress) {
+					// we supressed down, so report it now
+					*pvalue = 1;
+					rc = 1;
+					source->keymap[code].state = KEYSTATE_LONGPRESS_RELEASE;
+				}
+
+				else if(source->keymap[code].time>50) {
+					// we reported a longpress already
+					source->keymap[code].time=0;
+					source->keymap[code].repeat=false;
+					source->keymap[code].longpress = false;
+					source->keymap[code].state = KEYSTATE_RELEASED;
+				}
+			}
+			break;
+
+		case KEYSTATE_LONGPRESS_RELEASE:
+			// change to released
+			source->keymap[code].time=0;
+			source->keymap[code].repeat=false;
+			source->keymap[code].longpress = false;
+			source->keymap[code].state = KEYSTATE_RELEASED;
+			*pvalue = 0;
+			rc = 1;
+			break;
+
+		default:
+			ASSERT(0);
+	}
+
+	return rc;
+}
 
 #endif /* __DEV_KEYS_H */
