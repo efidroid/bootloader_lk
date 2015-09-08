780b9920a5df20ed87245c1e89b032d4a35e63fb
M1cha <sigmaepsilon92@gmail.com>
2015-09-08 13:04:28 +0200

    [EFIDROID] app: aboot: add 'EFIDROID_SAFEBOOT' support

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -1707,6 +1707,10 @@ void write_device_info_mmc(device_info *dev)
 	uint8_t lun = 0;
 	uint32_t ret = 0;
 
+	#ifdef EFIDROID_SAFEBOOT
+	return;
+	#endif
+
 	if (devinfo_present)
 		index = partition_get_index("devinfo");
 	else
@@ -1778,6 +1782,11 @@ void write_device_info_flash(device_info *dev)
 	struct device_info *info = memalign(PAGE_SIZE, ROUNDUP(BOOT_IMG_MAX_PAGE_SIZE, PAGE_SIZE));
 	struct ptentry *ptn;
 	struct ptable *ptable;
+
+	#ifdef EFIDROID_SAFEBOOT
+	return;
+	#endif
+
 	if(info == NULL)
 	{
 		dprintf(CRITICAL, "Failed to allocate memory for device info struct\n");
@@ -3732,6 +3741,10 @@ void aboot_init(const struct app_descriptor *app)
 	#if NO_KEYPAD_DRIVER
 	if (fastboot_trigger())
 		boot_into_fastboot = true;
+	#endif
+
+	#ifdef EFIDROID_SAFEBOOT
+	boot_into_fastboot = true;
 	#endif
 
 #if USE_PON_REBOOT_REG
