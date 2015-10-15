37eadf7b2fbab2788b67ba540f1ec922e90e88b2
Parth Dixit <parthd@codeaurora.org>
2015-10-15 12:37:41 +0530

    platform, target: msm8952: register app region based on tz version
    Add app address region for tz 4.0. Set the addresses
    based on TZ version.
    
    Change-Id: I3ae89b0f736d4a7fb19f4b76d41140805eaf0443

--- target/msm8952/init.c
+++ target/msm8952/init.c
@@ -56,6 +56,7 @@
 #include <clock.h>
 #include <secapp_loader.h>
 #include <rpmb.h>
+#include <qseecomi_lk.h>
 
 #include "target/display.h"
 
@@ -286,6 +287,10 @@ void target_init(void)
 		dprintf(CRITICAL, "Failed to initialize qseecom, error: %d\n", ret);
 		ASSERT(0);
 	}
+ 
+	/* set different address for TZ 4.0 */
+	if (qseecom_get_version() >= QSEE_VERSION_40)
+		qseecom_lk_set_app_region(APP_REGION_ADDR_TZ40, APP_REGION_SIZE_TZ40);
 
 	/* Start Qseecom */
 	ret = qseecom_tz_init();
