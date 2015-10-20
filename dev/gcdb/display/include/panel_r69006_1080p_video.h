9e5820a45e061a3616f0d120f73882cc54546338
Vishnuvardhan Prodduturi <vproddut@codeaurora.org>
2015-10-20 21:20:43 +0530

    target: msm8952: Add swire control support for AMOLED panels
    LAB/IBB voltages for AMOLED panels can be controlled by SWIRE signal
    from the panel instead of controlling explicitly in SW. This change
    adds support for SWIRE control.
    
    Conflicts:
    	dev/gcdb/display/include/panel_r69006_1080p_cmd.h
    	dev/gcdb/display/include/panel_r69006_1080p_video.h
    
    Change-Id: I01f43af194c02014259d43a3e736b94213afe7bf

--- dev/gcdb/display/include/panel_r69006_1080p_video.h
+++ dev/gcdb/display/include/panel_r69006_1080p_video.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -298,6 +298,10 @@ static const uint32_t r69006_1080p_video_timings[] = {
 
 static struct panel_timing r69006_1080p_video_timing_info = {
 	0, 4, 0x20, 0x2c
+};
+
+static struct labibb_desc r69006_1080p_video_labibb = {
+	0, 1, 5700000, 5700000, 5700000, 5700000, 3, 3, 1, 0
 };
 
 /*---------------------------------------------------------------------------*/
