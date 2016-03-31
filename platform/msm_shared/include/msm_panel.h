a5fb4daf3e4ea5e9d17875ef079b18919da44c19
Dhaval Patel <pdhaval@codeaurora.org>
2016-03-30 17:40:53 -0700

    display: add support for SCR rev for DSC 1.1
    Add support for SCR rev for DSC 1.1.
    
    Change-Id: I6a0e6c61e120a90d8e106b16f9b55782c02f69ac

--- platform/msm_shared/include/msm_panel.h
+++ platform/msm_shared/include/msm_panel.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -148,6 +148,7 @@ struct dsc_desc {
 
 	int major;
 	int minor;
+	int scr_rev;
 	int pps_id;
 
 	int pic_height;
