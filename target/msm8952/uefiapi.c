2f416a75d5dda02bc75a3c947b9b8c60b7373d52
M1cha <sigmaepsilon92@gmail.com>
2016-07-27 08:23:11 +0200

    [EFIDROID] msm8952: uefiapi: update

--- target/msm8952/uefiapi.c
+++ target/msm8952/uefiapi.c
@@ -1,29 +1,11 @@
 #include <err.h>
-#include <debug.h>
-#include <stdint.h>
-#include <mmc.h>
-#include <spmi.h>
 #include <board.h>
-#include <target.h>
+#include <stdint.h>
 #include <pm8x41.h>
-#include <qtimer.h>
 #include <dev/keys.h>
-#include <dev/fbcon.h>
-#include <mipi_dsi.h>
-#include <sdhci_msm.h>
-#include <target/display.h>
 #include <platform/iomap.h>
-#include <platform/clock.h>
-#include <platform/gpio.h>
-#include <partition_parser.h>
-#include <rpm-smd.h>
 
 #include <uefiapi.h>
-
-#define PMIC_ARB_CHANNEL_NUM    0
-#define PMIC_ARB_OWNER_ID       0
-
-static uint32_t pmic_ver;
 
 /////////////////////////////////////////////////////////////////////////
 //                                KEYS                                 //
@@ -64,71 +46,9 @@ static key_event_source_t event_source = {
 //                            PLATFORM                                 //
 /////////////////////////////////////////////////////////////////////////
 
-extern struct mmc_device *dev;
-
-void api_platform_early_init(void) {
-	// from platform_early_init, but without GIC
-	board_init();
-	platform_clock_init();
-	qtimer_init();
-	scm_init();
-
-	// UART
-	target_early_init();
-}
-
-void api_platform_init(void) {
-	// from target_init
-	// Initialize PMIC driver
-	spmi_init(PMIC_ARB_CHANNEL_NUM, PMIC_ARB_OWNER_ID);
-
-	/* Save PM8941 version info. */
-	pmic_ver = pm8x41_get_pmic_rev();
-
-	keys_init();
+void uefiapi_platform_init_post(void) {
 	keys_add_source(&event_source);
 	event_source.keymap[0].enable_longpress = true;
-}
-
-void api_platform_uninit(void) {
-	// from target_uninit
-
-	/*Keep the MMC card in sleep state before entering into kernel
-	so that kernel driver will do the initialization of the card again*/
-	mmc_put_card_to_sleep(dev);
-	sdhci_mode_disable(&dev->host);
-
-#if SMD_SUPPORT
-	rpm_smd_uninit();
-#endif
-}
-
-
-/////////////////////////////////////////////////////////////////////////
-//                            BlockIO                                  //
-/////////////////////////////////////////////////////////////////////////
-
-void target_sdc_init(void);
-
-#if SMD_SUPPORT
-static void rpm_smd_init_once(void) {
-	static int initialized = 0;
-	if(!initialized) {
-		rpm_smd_init();
-		initialized = 1;
-	}
-}
-#endif
-
-int api_mmc_init(void) {
-#if SMD_SUPPORT
-	// we can't do this in platform_init because this needs interrupts
-	rpm_smd_init_once();
-#endif
-
-	target_sdc_init();
-
-	return 0;
 }
 
 void* api_mmap_get_platform_mappings(void* pdata, lkapi_mmap_mappings_cb_t cb) {
