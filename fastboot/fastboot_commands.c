3d92ae5f030182ef43fa003fb89a5aab4960fd61
M1cha <sigmaepsilon92@gmail.com>
2016-08-05 19:50:24 +0200

    fastboot: use local hex2unsigned

--- fastboot/fastboot_commands.c
+++ fastboot/fastboot_commands.c
@@ -117,7 +117,33 @@ struct fastboot_cmd_desc {
     fastboot_cmd_fn cb;
 };
 
-unsigned hex2unsigned(const char *x);
+/* todo: give lk strtoul and nuke this */
+static unsigned hex2unsigned(const char *x)
+{
+    unsigned n = 0;
+
+    while(*x) {
+        switch(*x) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            n = (n << 4) | (*x - '0');
+            break;
+        case 'a': case 'b': case 'c':
+        case 'd': case 'e': case 'f':
+            n = (n << 4) | (*x - 'a' + 10);
+            break;
+        case 'A': case 'B': case 'C':
+        case 'D': case 'E': case 'F':
+            n = (n << 4) | (*x - 'A' + 10);
+            break;
+        default:
+            return n;
+        }
+        x++;
+    }
+
+    return n;
+}
 
 void cmd_oem_reboot_recovery(const char *arg, void *data, unsigned sz)
 {
