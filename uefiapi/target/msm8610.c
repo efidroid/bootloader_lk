c29b3818be674e5c168c1c65d19aff98838ef83a
M1cha <sigmaepsilon92@gmail.com>
2016-08-06 22:56:53 +0200

    uefiapi: target: reformat using spaces

--- uefiapi/target/msm8610.c
+++ uefiapi/target/msm8610.c
@@ -16,51 +16,55 @@ uint32_t target_volume_down(void);
 
 static int target_power_key(void)
 {
-	return pm8x41_get_pwrkey_is_pressed();
+    return pm8x41_get_pwrkey_is_pressed();
 }
 
-static int event_source_poll(key_event_source_t* source) {
-	uint16_t value = target_power_key();
-	if(keys_set_report_key(source, 0, &value)){
-		keys_post_event(13, value);
-	}
+static int event_source_poll(key_event_source_t *source)
+{
+    uint16_t value = target_power_key();
+    if (keys_set_report_key(source, 0, &value)) {
+        keys_post_event(13, value);
+    }
 
-	value = target_volume_up();
-	if(keys_set_report_key(source, 1, &value)){
-		keys_post_event(KEY_VOLUMEUP, value);
-	}
+    value = target_volume_up();
+    if (keys_set_report_key(source, 1, &value)) {
+        keys_post_event(KEY_VOLUMEUP, value);
+    }
 
-	value = target_volume_down();
-	if(keys_set_report_key(source, 2, &value)){
-		keys_post_event(KEY_VOLUMEDOWN, value);
-	}
+    value = target_volume_down();
+    if (keys_set_report_key(source, 2, &value)) {
+        keys_post_event(KEY_VOLUMEDOWN, value);
+    }
 
-	return NO_ERROR;
+    return NO_ERROR;
 }
 
 static key_event_source_t event_source = {
-	.poll = event_source_poll
+    .poll = event_source_poll
 };
 
 /////////////////////////////////////////////////////////////////////////
 //                            PLATFORM                                 //
 /////////////////////////////////////////////////////////////////////////
 
-void uefiapi_platform_init_post(void) {
-	keys_add_source(&event_source);
-	event_source.keymap[0].enable_longpress = true;
+void uefiapi_platform_init_post(void)
+{
+    keys_add_source(&event_source);
+    event_source.keymap[0].enable_longpress = true;
 }
 
-void* api_mmap_get_platform_mappings(void* pdata, lkapi_mmap_mappings_cb_t cb) {
-	pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
-	pdata = cb(pdata, SYSTEM_IMEM_BASE, SYSTEM_IMEM_BASE, 1*MB, LKAPI_MEMORY_DEVICE);
-	pdata = cb(pdata, MSM_SHARED_BASE, MSM_SHARED_BASE, 2*MB, LKAPI_MEMORY_WRITE_THROUGH);
+void *api_mmap_get_platform_mappings(void *pdata, lkapi_mmap_mappings_cb_t cb)
+{
+    pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, SYSTEM_IMEM_BASE, SYSTEM_IMEM_BASE, 1*MB, LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, MSM_SHARED_BASE, MSM_SHARED_BASE, 2*MB, LKAPI_MEMORY_WRITE_THROUGH);
 
-	return pdata;
+    return pdata;
 }
 
-void *api_mmap_get_platform_lkmem(void *pdata, lkapi_mmap_lkmem_cb_t cb) {
-	pdata = cb(pdata, MSM_SHARED_BASE, 2*MB);
+void *api_mmap_get_platform_lkmem(void *pdata, lkapi_mmap_lkmem_cb_t cb)
+{
+    pdata = cb(pdata, MSM_SHARED_BASE, 2*MB);
 
-	return pdata;
+    return pdata;
 }
--- uefiapi/target/msm8960.c
+++ uefiapi/target/msm8960.c
@@ -13,37 +13,40 @@
 
 static int target_power_key(void)
 {
-	uint8_t ret = 0;
+    uint8_t ret = 0;
 
-	pm8921_pwrkey_status(&ret);
-	return ret;
+    pm8921_pwrkey_status(&ret);
+    return ret;
 }
 
-static int event_source_poll(key_event_source_t* source) {
-	uint16_t value = target_power_key();
-	if(keys_set_report_key(source, 0, &value)){
-		keys_post_event(13, value);
-	}
+static int event_source_poll(key_event_source_t *source)
+{
+    uint16_t value = target_power_key();
+    if (keys_set_report_key(source, 0, &value)) {
+        keys_post_event(13, value);
+    }
 
-	return NO_ERROR;
+    return NO_ERROR;
 }
 
 static key_event_source_t event_source = {
-	.poll = event_source_poll
+    .poll = event_source_poll
 };
 
 /////////////////////////////////////////////////////////////////////////
 //                            PLATFORM                                 //
 /////////////////////////////////////////////////////////////////////////
 
-void uefiapi_platform_init_post(void) {
-	keys_add_source(&event_source);
-	event_source.keymap[0].enable_longpress = true;
+void uefiapi_platform_init_post(void)
+{
+    keys_add_source(&event_source);
+    event_source.keymap[0].enable_longpress = true;
 }
 
-void* api_mmap_get_platform_mappings(void* pdata, lkapi_mmap_mappings_cb_t cb) {
-	pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
-	pdata = cb(pdata, MSM_IMEM_BASE, MSM_IMEM_BASE, 1*MB, LKAPI_MEMORY_DEVICE);
+void *api_mmap_get_platform_mappings(void *pdata, lkapi_mmap_mappings_cb_t cb)
+{
+    pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, MSM_IMEM_BASE, MSM_IMEM_BASE, 1*MB, LKAPI_MEMORY_DEVICE);
 
-	return pdata;
+    return pdata;
 }
