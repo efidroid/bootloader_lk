741cbceaa90762635c9a16a744c748844a1acc16
M1cha <sigmaepsilon92@gmail.com>
2016-08-06 23:09:49 +0200

    uefiapi: shim: reformat using spaces

--- uefiapi/shim.c
+++ uefiapi/shim.c
@@ -36,18 +36,18 @@ void display_image_on_screen(void)
 //
 status_t mask_interrupt(unsigned int vector)
 {
-	ASSERT(uefiapi.int_mask);
-	return uefiapi.int_mask(vector);
+    ASSERT(uefiapi.int_mask);
+    return uefiapi.int_mask(vector);
 }
 
 status_t unmask_interrupt(unsigned int vector)
 {
-	ASSERT(uefiapi.int_unmask);
-	return uefiapi.int_unmask(vector);
+    ASSERT(uefiapi.int_unmask);
+    return uefiapi.int_unmask(vector);
 }
 
 void register_int_handler(unsigned int vector, int_handler func, void *arg)
 {
-	ASSERT(uefiapi.int_register_handler);
-	uefiapi.int_register_handler(vector, (lkapi_int_handler)func, arg);
+    ASSERT(uefiapi.int_register_handler);
+    uefiapi.int_register_handler(vector, (lkapi_int_handler)func, arg);
 }
