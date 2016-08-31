4489e898b333937f9fbc79b43bc2eee1434cfb07
M1cha <sigmaepsilon92@gmail.com>
2016-08-31 17:00:05 +0200

    fastboot: add cmdline extension when booting

--- fastboot/fastboot_commands.c
+++ fastboot/fastboot_commands.c
@@ -697,6 +697,9 @@ static void cmd_boot(const char *arg, void *data, unsigned sz)
             // update loading addresses
             update_ker_tags_rdisk_addr(&context, IS_ARM64(context.kernel_data));
 
+            // add to cmdline
+            libboot_cmdline_addall(&context.cmdline, lkargs_get_command_line(), 1);
+
             // prepare for boot
             rc = libboot_prepare(&context);
             if (!rc) {
