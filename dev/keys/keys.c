db2ca082bb320aef7b0bd537f13f17bbf1f09bc2
M1cha <sigmaepsilon92@gmail.com>
2016-10-10 15:55:40 +0200

    Revert "[EFIDROID] dev: keys: add key queue"
    This reverts commit 8fb4f54eb38dedea6be9cbf11f393edbcab3555a.

--- dev/keys/keys.c
+++ dev/keys/keys.c
@@ -29,158 +29,39 @@
  * SUCH DAMAGE.
  */
 
-#include <err.h>
 #include <bits.h>
-#include <list.h>
 #include <debug.h>
 #include <string.h>
-#include <malloc.h>
 #include <dev/keys.h>
 
-#define LOCAL_TRACE 0
-
 static unsigned long key_bitmap[BITMAP_NUM_WORDS(MAX_KEYS)];
-
-typedef struct {
-	struct list_node node;
-	uint16_t code;
-	uint16_t value;
-} key_event_t;
-
-static struct list_node event_queue;
-static struct list_node event_sources;
-
-int keys_poll(void)
-{
-	key_event_source_t *source;
-	list_for_every_entry(&event_sources, source, key_event_source_t, node) {
-		// update time
-		time_t current = current_time();
-		source->delta = current-source->last;
-		source->last = current;
-
-		source->poll(source);
-	}
-
-	return 0;
-}
 
 void keys_init(void)
 {
 	memset(key_bitmap, 0, sizeof(key_bitmap));
-	list_initialize(&event_queue);
-	list_initialize(&event_sources);
 }
 
-void keys_add_source(key_event_source_t* source) {
-	int i;
-
-	source->last = INFINITE_TIME;
-
-	for(i=0; i<MAX_KEYS; i++) {
-		source->keymap[i].time = 0;
-		source->keymap[i].repeat = false;
-		source->keymap[i].longpress = false;
-		source->keymap[i].state = KEYSTATE_RELEASED;
-	}
-
-	list_add_tail(&event_sources, &source->node);
-}
-
-int keys_post_event(uint16_t code, int16_t value)
+void keys_post_event(uint16_t code, int16_t value)
 {
-	int i;
-
 	if (code >= MAX_KEYS) {
-		LTRACEF("Invalid keycode posted: %d\n", code);
-		return ERR_INVALID_ARGS;
+		dprintf(INFO, "Invalid keycode posted: %d\n", code);
+		return;
 	}
 
+	/* TODO: Implement an actual event queue if it becomes necessary */
 	if (value)
 		bitmap_set(key_bitmap, code);
 	else
 		bitmap_clear(key_bitmap, code);
 
-	// signal
-	LTRACEF("key state change: %d %d\n", code, value);
-
-	if(!value) return NO_ERROR;
-
-	uint8_t seq[3];
-	uint8_t seqsz = 0;
-
-	if(code<=0xff) {
-		seq[seqsz++] = code;
-	}
-	else {
-		switch(code) {
-			case KEY_VOLUMEUP:
-			seq[seqsz++] = 0x1b;
-			seq[seqsz++] = 0x5b;
-			seq[seqsz++] = 0x41;
-			break;
-
-			case KEY_VOLUMEDOWN:
-			seq[seqsz++] = 0x1b;
-			seq[seqsz++] = 0x5b;
-			seq[seqsz++] = 0x42;
-			break;
-		}
-	}
-
-	for(i=0; i<seqsz; i++) {
-		// create event
-		key_event_t* event = malloc(sizeof(key_event_t));
-		event->code = seq[i];
-		event->value = value;
-
-		// add event
-		list_add_tail(&event_queue, &event->node);
-	}
-
-	return NO_ERROR;
+//	dprintf(INFO, "key state change: %d %d\n", code, value);
 }
 
 int keys_get_state(uint16_t code)
 {
 	if (code >= MAX_KEYS) {
-		LTRACEF("Invalid keycode requested: %d\n", code);
-		return ERR_INVALID_ARGS;
+		dprintf(INFO, "Invalid keycode requested: %d\n", code);
+		return -1;
 	}
-
 	return bitmap_test(key_bitmap, code);
-}
-
-void keys_clear_all(void)
-{
-	memset(key_bitmap, 0, sizeof(key_bitmap));
-
-	while(keys_has_next()) {
-		key_event_t* event = list_remove_tail_type(&event_queue, key_event_t, node);
-		free(event);
-	}
-}
-
-int keys_get_next(uint16_t* code, uint16_t* value)
-{
-	// wait for next event
-	if(!keys_has_next())
-		return ERR_NOT_READY;
-
-	// get event
-	key_event_t* event = list_remove_head_type(&event_queue, key_event_t, node);
-
-	// set result code
-	*code = event->code;
-	*value = event->value;
-
-	// free event
-	free(event);
-
-	return NO_ERROR;
-}
-
-int keys_has_next(void)
-{
-	return !list_is_empty(&event_queue);
 }
