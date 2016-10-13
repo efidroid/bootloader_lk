9af6d738f434c0067ce46036fe9ef888470b03ca
M1cha <sigmaepsilon92@gmail.com>
2016-10-13 09:43:35 +0200

    uefiapi: target: msm8916: reformat code

--- uefiapi/target/msm8916.c
+++ uefiapi/target/msm8916.c
@@ -16,43 +16,47 @@ uint32_t target_volume_down(void);
 
 static int target_power_key(void)
 {
-	return pm8x41_get_pwrkey_is_pressed();
+    return pm8x41_get_pwrkey_is_pressed();
 }
 
-static int event_source_poll(newkey_event_source_t* source) {
+static int event_source_poll(newkey_event_source_t *source)
+{
     newkeys_set_report_key(source, 13, target_power_key());
     newkeys_set_report_key(source, KEY_VOLUMEUP, target_volume_up());
     newkeys_set_report_key(source, KEY_VOLUMEDOWN, target_volume_down());
 
-	return NO_ERROR;
+    return NO_ERROR;
 }
 
 static newkey_event_source_t event_source = {
-	.poll = event_source_poll
+    .poll = event_source_poll
 };
 
 /////////////////////////////////////////////////////////////////////////
 //                            PLATFORM                                 //
 /////////////////////////////////////////////////////////////////////////
 
-void uefiapi_platform_init_post(void) {
-	newkeys_add_source(&event_source);
+void uefiapi_platform_init_post(void)
+{
+    newkeys_add_source(&event_source);
 }
 
-void* api_mmap_get_platform_mappings(void* pdata, lkapi_mmap_mappings_cb_t cb) {
-	pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
-	pdata = cb(pdata, A53_SS_BASE, A53_SS_BASE, (A53_SS_END - A53_SS_BASE), LKAPI_MEMORY_DEVICE);
-	pdata = cb(pdata, SYSTEM_IMEM_BASE, SYSTEM_IMEM_BASE, 1*MB, LKAPI_MEMORY_WRITE_THROUGH);
-	pdata = cb(pdata, MSM_SHARED_BASE, MSM_SHARED_BASE, 1*MB, LKAPI_MEMORY_WRITE_THROUGH);
+void *api_mmap_get_platform_mappings(void *pdata, lkapi_mmap_mappings_cb_t cb)
+{
+    pdata = cb(pdata, MSM_IOMAP_BASE, MSM_IOMAP_BASE, (MSM_IOMAP_END - MSM_IOMAP_BASE), LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, A53_SS_BASE, A53_SS_BASE, (A53_SS_END - A53_SS_BASE), LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, SYSTEM_IMEM_BASE, SYSTEM_IMEM_BASE, 1*MB, LKAPI_MEMORY_WRITE_THROUGH);
+    pdata = cb(pdata, MSM_SHARED_BASE, MSM_SHARED_BASE, 1*MB, LKAPI_MEMORY_WRITE_THROUGH);
 #ifdef RPMB_SND_RCV_BUF
-	pdata = cb(pdata, RPMB_SND_RCV_BUF, RPMB_SND_RCV_BUF, RPMB_SND_RCV_BUF_SZ*MB, LKAPI_MEMORY_DEVICE);
+    pdata = cb(pdata, RPMB_SND_RCV_BUF, RPMB_SND_RCV_BUF, RPMB_SND_RCV_BUF_SZ*MB, LKAPI_MEMORY_DEVICE);
 #endif
 
-	return pdata;
+    return pdata;
 }
 
-void *api_mmap_get_platform_lkmem(void *pdata, lkapi_mmap_lkmem_cb_t cb) {
-	pdata = cb(pdata, MSM_SHARED_BASE, 1*MB);
+void *api_mmap_get_platform_lkmem(void *pdata, lkapi_mmap_lkmem_cb_t cb)
+{
+    pdata = cb(pdata, MSM_SHARED_BASE, 1*MB);
 #ifdef RPMB_SND_RCV_BUF
     pdata = cb(pdata, RPMB_SND_RCV_BUF, RPMB_SND_RCV_BUF_SZ*MB);
 #endif
